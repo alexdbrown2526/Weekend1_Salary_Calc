8106d8ccfee3430e7a9fa5187ffeb40e8c20e4a6
Alex Brown <alexdbrown2526@gmail.com>
2018-09-03 19:56:54 -0500

    wknd 1

--- client.js
+++ client.js
@@ -2,41 +2,68 @@ console.log('js');
 
 $(document).ready(readyNow);
 
-function readyNow(){
-    $('#submitButton').on('click', function(handleClick){
-        
-        
-        
-        
-        console.log('button working');
-    })
+let monthlyTotal = 0;
+
+$(document).on('click', '.delete-button', function(){
+    $(this).parent().parent().remove();
+    
+    console.log(this);
+
+})
+
+function readyNow() {
+    $('#submitButton').on('click', handleClick);
+    
+    console.log('button working');
 }
 
 function handleClick() {
+    $('#totalMonthly').empty()
     let firstName = $('#firstNameInput').val()
     let lastName = $('#lastNameInput').val()
     let identity = $('#identityInput').val()
     let title = $('#titleInput').val()
-    let annualSalary = $('#annualSalary').val()
+    let annualSalary = $('#annualSalaryInput').val()
 
-    $('#employeeBody').append(`
+    
+  
+    
+
+
+    $('#employeeTableBody').append(`
     <tr>
-        <td>`+ firstName + `</td>
-        <td>`+ lastName + `</td>
-        <td>`+ identity + `</td>
-        <td>`+ title + `</td>
-        <td>`+ annualSalary +`</td>
-
+        <td>` + firstName + `</td>
+        <td>` + lastName + `</td>
+        <td>` + identity + `</td>
+        <td>` + title + `</td>
+        <td>` + annualSalary + `</td>
+        <td> <button class="delete-button">Delete</button> </td>
     </tr>
 `);
 
-    $('#firstNameInput').val()
-    $('#lastNameInput').val()
-    $('#identityInput').val()
-    $('#titleInput').val()
-    $('annualSalary').val()
+
+    calculateMonthly();
+    $('#totalMonthly').append('<footer>' + monthlyTotal + '</footer')
+
+    $('#firstNameInput').val('')
+    $('#lastNameInput').val('')
+    $('#identityInput').val('')
+    $('#titleInput').val('')
+    $('#annualSalary').val('')
 
 }
+
+function calculateMonthly() {
+    let annualSalary = $('#annualSalaryInput').val()
+    monthlyTotal = monthlyTotal + (annualSalary / 12);
+
+    if (
+        monthlyTotal > 20000) {
+            $('#totalMonthly').css('background-color', 'red');
+    }
+}
+
+
 
 
 
